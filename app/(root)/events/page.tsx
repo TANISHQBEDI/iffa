5d4e0b3d5b11870b5b9c99a9a7c28478fb0cc331
Tanishq Bedi <66412510+TANISHQBEDI@users.noreply.github.com>
2025-10-29 02:57:42 +1100

    Completed the responsive footer component and some optimizations in the display card image loading

--- app/(root)/events/page.tsx
+++ app/(root)/events/page.tsx
@@ -1,4 +1,10 @@
-import React from 'react'
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+  title: "Past Events - IFFA Awards",
+  description:
+    "Explore the past events of the IFFA Awards and relive the unforgettable moments.",
+}
 
 const Events = () => {
   return (
--- lib/constants.ts
+++ lib/constants.ts
@@ -26,3 +26,34 @@ export const NAV_ITEMS: NavItem[] = [
 export const PLAYLIST_ID = process.env.PLAYLIST_ID;
 
 export const API_BASE_URL = process.env.API_BASE_URL;
+
+export const FOOTER_ACKNOWLEDGEMENT = `We acknowledge the Traditional Owners of the land where we work and live. 
+We pay our respects to Elders past, present and emerging. 
+We celebrate the stories, culture and traditions of Aboriginal and Torres Strait Islander Elders of all communities 
+who also work and live on this land.`
+
+export const FOOTER_LINKS = [
+    {
+        label: 'About',
+        href: '/about'
+    },
+    {
+        label: 'Contact',
+        href: '/contact-us'
+    },
+    {
+        label: 'IFFA Global',
+        href: '/ambassadors'
+    },
+    {
+        label: 'Partner with us',
+        href: '/partner-with-us'
+    }
+]
+
+export const FOOTER_SOCIALS: SocialLink[] = [
+    { label: 'Facebook', href: 'https://www.facebook.com/iffaawardss', icon: 'facebook' },
+    { label: 'Instagram', href: 'https://www.instagram.com/iffaawards/', icon: 'instagram' },
+    { label: 'LinkedIn', href: 'https://www.linkedin.com/in/iffaawards/', icon: 'linkedin' },
+    { label: 'YouTube', href: 'https://www.youtube.com/channel/UCO2xJ6Cw1-5o1iolIJtO4yQ', icon: 'youtube' },
+]
